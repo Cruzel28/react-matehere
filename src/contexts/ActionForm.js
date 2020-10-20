249e0c9037f083243b758f47147d31a30af5d7f3
DogeCnx <Saratoga1412@gmail.com>
2020-10-20 15:00:54 +0700

    ADD context/ActionForm

---
+++ src/contexts/ActionForm.js
@@ -0,0 +1,25 @@
+import React, { useState, createContext } from 'react'
+
+export const ActionForm = createContext({})
+
+export const ActionFormProvider = ({ children }) => {
+
+    const [activeStep, setActiveStep] = useState(0);
+
+    const [ infoHavePost, setInfoHavePost ] = useState({ TestInfo1 : undefined,TestInfo2 : undefined})
+    const [infoNeedPost, setInfoNeedPost] = useState({ TestInfo : undefined,TestInfo2 : undefined})
+
+    const InfoPost = {
+        HavePost : { infoHavePost :infoHavePost, setInfoHavePost : setInfoHavePost},
+        NeedPost : { infoNeedPost :infoNeedPost, setInfoNeedPost : setInfoNeedPost },
+        Active : { activeStep: activeStep, setActiveStep :setActiveStep}
+        
+    }
+
+        
+
+    return <ActionForm.Provider value={InfoPost}> {children} </ActionForm.Provider>
+}
+
+
+export default ActionForm;
