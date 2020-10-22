d3d9eebcb8e9dd46bb24cdff8e374cb19dffc18c
DogeCnx <Saratoga1412@gmail.com>
2020-10-22 16:25:23 +0700

    ADD post.js in /page

---
+++ src/pages/Post.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from '../components/Form'
+
+
+function Post() {
+    return (
+        <>
+            <Form />
+        </>
+    );
+}
+
+export default Post;
