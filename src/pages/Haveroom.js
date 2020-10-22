767893e7ca42e04b99092af00cb1f142fbac45c8
Cruzel28 <gin_ko28@hotmail.com>
2020-10-23 01:13:04 +0700

    Add  Haveroom.js

---
+++ src/pages/Haveroom.js
@@ -0,0 +1,19 @@
+import React from "react";
+import BackgroundBlock from "../components/BackgroundBlock";
+
+import { Title, Subtitle, SemiSubtitle } from "../components/Typography";
+import styled from "styled-components";
+
+function Haveroom() {
+    return (
+      <>
+        <div>
+          <BackgroundBlock>
+          
+          </BackgroundBlock>
+        </div>
+      </>
+    );
+  }
+  
+  export default Haveroom;
