fa01b0a9874c25e7d23e58954961a6e286237cd4
Cruzel28 <gin_ko28@hotmail.com>
2020-10-22 17:02:55 +0700

    Update Typography.js

--- src/components/Typography.js
+++ src/components/Typography.js
@@ -9,11 +9,12 @@ margin : .5rem;
 export const Title = styled(Text)`
 font-size : 3rem;
 font-weight: 900;
+margin-bottom: 2.5rem;
 
 
 `
 export  const Subtitle = styled(Text)`
-font-size : 1rem;
+font-size : 1.5rem;
 padding-bottom: 1rem;
 `
 
