7e0afe41ad58fbdf12644342149f6248545e6a83
DogeCnx <Saratoga1412@gmail.com>
2020-10-22 16:27:16 +0700

    Update state for test

--- src/contexts/ActionForm.js
+++ src/contexts/ActionForm.js
@@ -6,13 +6,23 @@ export const ActionFormProvider = ({ children }) => {
 
     const [activeStep, setActiveStep] = useState(0);
 
-    const [ infoHavePost, setInfoHavePost ] = useState({ TestInfo1 : undefined,TestInfo2 : undefined})
-    const [infoNeedPost, setInfoNeedPost] = useState({ TestInfo : undefined,TestInfo2 : undefined})
+    const [ infoHavePost, setInfoHavePost ] = useState({ 
+
+        TestInfo1 : undefined,TestInfo2 : undefined
+    })
+
+    const [infoNeedPost, setInfoNeedPost] = useState({ 
+        
+        TestInfo : undefined,TestInfo2 : undefined
+    })
+
+    const [ test, settest] = useState({ Email : undefined})
 
     const InfoPost = {
         HavePost : { infoHavePost :infoHavePost, setInfoHavePost : setInfoHavePost},
         NeedPost : { infoNeedPost :infoNeedPost, setInfoNeedPost : setInfoNeedPost },
-        Active : { activeStep: activeStep, setActiveStep :setActiveStep}
+        Active : { activeStep: activeStep, setActiveStep :setActiveStep},
+        Test : {test: test, settest :settest}
         
     }
 
