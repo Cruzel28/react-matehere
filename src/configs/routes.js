bd45a75375838b46f20cdc30a7f067a8e57a02ef
Cruzel28 <gin_ko28@hotmail.com>
2020-10-23 01:14:29 +0700

    Update routes.js

--- src/configs/routes.js
+++ src/configs/routes.js
@@ -11,6 +11,18 @@ export default {
     path: '/addpost',
     exact : true,
     component : lazy( () => import('../pages/Post'))
+  },
+
+  Needroom : {
+    path: '/needroom',
+    exact : true,
+    component : lazy( () => import('../pages/Needroom'))
+  },
+
+  Haveroom : {
+    path: '/haveroom',
+    exact : true,
+    component : lazy( () => import('../pages/Haveroom'))
   }
 
 }
