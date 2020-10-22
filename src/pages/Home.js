bcbcabcf11aab92aedfdae5557d69600831583b7
Cruzel28 <gin_ko28@hotmail.com>
2020-10-22 17:03:32 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,145 +1,171 @@
-import React from 'react' ;
-import BackgroundBlock from '../components/BackgroundBlock'
+import React from "react";
+import BackgroundBlock from "../components/BackgroundBlock";
+import HomeBackground from "../components/HomeBackground";
 
-import backgroundImage1 from '../assets/bg.jpg'
-import searchIcon from '../assets/search.png'
+import searchIcon from "../assets/search.png";
+import havePostIcon from "../assets/haveRoom.jpg";
+import NeedPostIcon from "../assets/needRoom.jpg";
 
-
-import {Title ,Subtitle, SemiSubtitle} from '../components/Typography'
-import styled from 'styled-components'
+import { Title, Subtitle, SemiSubtitle } from "../components/Typography";
+import styled from "styled-components";
 
 const TextBox = styled.div`
+  position: absolute;
+  width: 70%;
+  height: 75%;
+  border: 2px solid #fff;
 
-position : absolute;
-  width : 80%;
-  height : 75%;
-  border : 2px solid #fff;
-  
-  display:flex;
+  display: flex;
   flex: 1;
-  flex-direction :column;
-  justify-content :center ;
-  align-items : center ;
- 
-`
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const SearchBar = styled.div`
   display: flex;
-`
+`;
 
 const InputBox = styled.input`
-width: 100%;
-height: fit-content;
+  width: 100%;
+  height: fit-content;
   box-sizing: border-box;
   border: 2px solid #ccc;
- 
-  font-family : 'Kanit', sans-serif;
+
+  font-family: "Kanit", sans-serif;
   font-size: 16px;
   background-color: #fff;
-  padding: .5rem 1rem;
+  padding: 0.5rem 1rem;
 
-  &::placeholder{
+  &::placeholder {
     color: #ccc;
   }
-`
+`;
 
 const SearchDropdown = styled.select`
-display : flex;
-    width: auto;
-  padding: 1rem ;
+  display: flex;
+  width: auto;
+  padding: 1rem;
   border: none;
-  background-color: #4192B0;
+  background-color: #4192b0;
 
   color: #fff;
 
-  font-family : 'Kanit', sans-serif;
+  font-family: "Kanit", sans-serif;
   font-size: 1rem;
 
-  &:hover{
-    background-color: #F96C01;
+  &:hover {
+    background-color: #f96c01;
     cursor: pointer;
   }
 
-  option{
-    padding: 1rem ;
+  option {
+    padding: 1rem;
     margin: 1rem;
     background-color: #fff;
-    color :#000;
+    color: #000;
 
-    &:hover{
-      background-color: #FFF0CB;
+    &:hover {
+      background-color: #fff0cb;
       cursor: pointer;
     }
   }
-
-`
+`;
 
 const Button = styled.button`
-display : flex;
-    
-  padding: 1rem ;
+  display: flex;
+  width: 20%;
+  height: -1rem;
+  padding: 1rem;
   border: none;
-  background-color: #4192B0;
-  color: #fff;
+  background-color: #4192b0;
+  background-image: url(${(props) => props.src});
+  background-repeat: no-repeat;
+  background-size: 1.5rem 2rem;
+  background-position: center;
 
-  font-family : 'Kanit', sans-serif;
-  font-size: 1rem;
-
-  &:hover{
-    background-color: #F96C01;
+  &:hover {
+    background-color: #f96c01;
     cursor: pointer;
   }
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Content = styled.button`
+  display: flex;
+  flex: 2;
+  width: 100%;
+  padding: 10rem;
+  margin: 2rem 5rem;
+  border: none;
+  background-color: #4192b0;
+  background-image: url(${(props) => props.src});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Lane = styled.div`
+  flex: 1;
+display: flex;
+flex-direction:column;
+align-items: center;
+  padding: 0 1rem;
 `
 
-
-
-
-const BackgroundImage1 = styled.div`
-
-  height : 100vh;
-  width : auto ;
-  background-image : url(${props => props.src});
-  background-repeat : no-repeat;
-  background-size : cover ;
-  background-position : center ;
-  background-attachment :fixed ;
-  
-  position : relative;
-
-`
-
-
-function Home () {
-  return <>
-        <div>
+function Home() {
+  return (
+    <>
+      <div>
         <BackgroundBlock>
           <TextBox>
-          <Subtitle>ค้นหารูมเมทได้ง่าย ๆ</Subtitle>
-          <Subtitle>ด้วย</Subtitle>
-          <Title>MateHere</Title>
-          <Subtitle>ค้นหาเลย !</Subtitle>
+            <Subtitle>ค้นหารูมเมทได้ง่าย ๆ</Subtitle>
+            <Subtitle>ด้วย</Subtitle>
+            <Title>MateHere</Title>
+            <Subtitle>ค้นหาเลย !</Subtitle>
 
-          <SearchBar>
-          <SearchDropdown>
-            <option>ต้องการหารูมเมท</option>
-            <option>ต้องการหาห้องและรูมเมท</option>
-          </SearchDropdown>
-          <InputBox placeholder="ตัวอย่าง: หน้ามอ, 2คน, ตีนดอย"/>
-          <Button>ค้นหา</Button>
-          </SearchBar>
-          <SemiSubtitle> หรือ 
-            <a href="#content">  กดที่นี่เพื่อสร้างโพสต์</a>
-          </SemiSubtitle>
+            <SearchBar>
+              <SearchDropdown>
+                <option>ต้องการหารูมเมท</option>
+                <option>ต้องการหาห้องและรูมเมท</option>
+              </SearchDropdown>
+              <InputBox placeholder="ตัวอย่าง: หน้ามอ, 2คน, ตีนดอย" />
+              <Button src={searchIcon} />
+            </SearchBar>
+            <SemiSubtitle>
+              {" "}
+              หรือ
+              <a href="#content"> กดที่นี่เพื่อสร้างโพสต์</a>
+            </SemiSubtitle>
           </TextBox>
-
-
         </BackgroundBlock>
-          <BackgroundImage1  id="content" src={backgroundImage1}>
+        <HomeBackground>
+          <ContentContainer id="content">
+            <Subtitle>สร้างโพสต์</Subtitle>
+          </ContentContainer>
+          <ContentContainer>
+            <Lane>
+            <Content  src={havePostIcon} />
+            <Subtitle>หารูมเมท</Subtitle>
+            </Lane>
             
-          </BackgroundImage1>
-        </div>
+            <Lane>
+            <Content src={NeedPostIcon} />
+            <Subtitle>หาห้องและรูมเมท</Subtitle>
+            </Lane>
+          </ContentContainer>
+        </HomeBackground>
+      </div>
     </>
-
+  );
 }
 
 export default Home;
