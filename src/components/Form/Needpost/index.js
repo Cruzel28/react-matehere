94c0823fc1cd12b21354f70e235f3dec0f8382f7
DogeCnx <Saratoga1412@gmail.com>
2020-10-22 19:26:23 +0700

    Update Needpost index init page

--- src/components/Form/Needpost/index.js
+++ src/components/Form/Needpost/index.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import { Formik, Form, Field} from 'formik'
+import * as Yup from 'yup';
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button';
+import styled from 'styled-components'
+
+import ActionForm from '../../../contexts/ActionForm'
+
+const NeedPostSchema = Yup.object().shape({
+
+});
+
 
 export function NeedPost1() {
+
+    //state global Step
+    const {Active,Steps,HandleButton,NeedPost } = useContext(ActionForm );
+
+    const {activeStep } = Active;
+    //state function handle event buttonClick steponForm );
+    const { handleNext,handleBack  } = HandleButton;
+
+    const { steps } = Steps
+
+    //ActionContext Form Havepost Information State 
+    const { infoNeedPost , setInfoNeedPost } = NeedPost;
+
     return (
-        <div>
-            Need 1
-        </div>
+        
+        <>
+            <Formik
+                    initialValues={{ //กำหนด initialValues
+  
+                    }}
+                    validationSchema={NeedPostSchema} //กำหนด validationSchema
+                    
+                    onSubmit={values => {
+                        setInfoNeedPost()
+                      }}
+            >
+                { props => (
+                    <Form >
+
+                        <Button disabled={activeStep === 0} onClick={handleBack}>Back</Button>
+                        <Button  onClick={handleNext}> {activeStep === steps.length - 1 ? 'Finish' : 'Next'} </Button>
+                    </Form>
+                )}
+            </Formik>
+        </>
     );
 }
 export function NeedPost2() {
