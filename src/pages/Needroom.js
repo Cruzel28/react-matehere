c0f482e85c32ae9f950fc93a2f14992581498a29
Cruzel28 <gin_ko28@hotmail.com>
2020-10-23 01:13:59 +0700

    Add  Needroom.js

---
+++ src/pages/Needroom.js
@@ -0,0 +1,19 @@
+import React from "react";
+import BackgroundBlock from "../components/BackgroundBlock";
+
+import { Title, Subtitle, SemiSubtitle } from "../components/Typography";
+import styled from "styled-components";
+
+function Needroom() {
+    return (
+      <>
+        <div>
+          <BackgroundBlock>
+          
+          </BackgroundBlock>
+        </div>
+      </>
+    );
+  }
+  
+  export default Needroom;
