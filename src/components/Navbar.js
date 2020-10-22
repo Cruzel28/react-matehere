c23ba9479bde086eaafa91f841a4774b2aa66c44
Cruzel28 <gin_ko28@hotmail.com>
2020-10-22 17:57:48 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from 'react'
+import React from 'react'
 import logoImage from '../assets/logo.png'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
