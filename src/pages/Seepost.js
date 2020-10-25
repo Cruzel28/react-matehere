b9f3c2536f67fd66e3b90c03ad306dc2b65f7b2d
Cruzel28 <gin_ko28@hotmail.com>
2020-10-25 21:22:03 +0700

    Add Seepost.js

---
+++ src/pages/Seepost.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import  BackgroundBlock from '../components/BackgroundBlock';
+import {Container} from '../components/Form/index'
+
+function Seepost() {
+
+
+    return (
+        <>
+        <BackgroundBlock>
+            <Container>
+            
+            </Container>
+            </BackgroundBlock>
+ 
+        </>
+    );
+}
+
+export default Seepost
