9ff117462e28be928615c93ed0779c07543b21f2
Cruzel28 <gin_ko28@hotmail.com>
2020-10-28 03:45:44 +0700

    Update Have/Needroom.js

--- src/pages/Haveroom.js
+++ src/pages/Haveroom.js
@@ -8,37 +8,48 @@ import { TitlePost, SemiSubtitle } from "../components/Typography";
 import styled from "styled-components";
 
 const Container = styled.div`
-   position: absolute;
-  width: auto;
+  position: absolute;
+  width: 60%;
   height: auto;
-  background-color:#81C0C5;
-  display: flex;
-  flex: 1;
-  flex-direction: row;
- 
-   
-`
+  background-color: #81c0c5;
+
+`;
 const Content = styled.div`
   display: flex;
   flex: 1;
   flex-direction: column;
-
-`
+`;
 const LayoutContent = styled.div`
- flex: 1;
-display: flex;
-flex-direction:row;
-align-items: center;
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
   padding: 1rem 1rem;
-`
+
+`;
 
 const Button = styled.button`
+  display: flex;
+  flex:1;
+
+  width: 45%;
+  padding: 4rem;
+  margin: 2rem 2rem;
+  border: none;
+  background-image: url(${(props) => props.src});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+   cursor: pointer;
+`
+
+const AddButton = styled.button`
 display: flex;
 
 
   width: 45%;
   padding: 4.5rem;
-  margin: 2rem 5rem;
+  margin: 2rem 2.5rem;
   border: none;
   background-color: #4192b0;
   background-image: url(${(props) => props.src});
@@ -52,8 +63,31 @@ display: flex;
     transition: filter 0.5s;
   }
 `;
+const Card = styled.div`
+
+box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+
+  background-color:#004B62;
+  margin: 0 1rem;
+
+  &:hover{
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  }
+`
+const Description = styled.div`
+  padding-bottom: 1rem;
+  padding-left: 1rem;
+`
 
 function Haveroom() {
+  // const [page, setPage] = useState(1);
+  // const [dataAll, setDataAll] = useState(null);
+
+  // useEffect(() => {
+  //   getNeedpost(page).then((response) => setDataAll(response));
+  // }, [page]);
+
     return (
       <>
         <div>
@@ -71,11 +105,52 @@ function Haveroom() {
               <LayoutContent>
 
                 <Link to="/addpost">
-              <Button src={AddPost}/>
+              <AddButton src={AddPost}/>
               </Link>
-              <Link to="/seepost">
-              <Button src={Post}/>
-              </Link>
+
+                {/* {dataAll?.data.data.map((item, index) => (
+                  <Link to={`/seepost/${index}`}>
+                    <Button src={item.cover_img_url} />
+                  <Description>ประเภท :{item.need_type}<p/>ที่อยู่{item.need_location}</Description>
+                  </Link>
+                ))} */}
+                    <Card>
+                    <Link to={`/seepost/1`}>
+                    <Button src={AddPost} />
+                  <Description>ประเภท :<p/>ที่อยู่dssds</Description>
+                  </Link>
+                  </Card>
+
+                  <Card>
+                    <Link to={`/seepost/1`}>
+                    <Button src={AddPost} />
+                  <Description>ประเภท :<p/>ที่อยู่dssds</Description>
+                  </Link>
+                  </Card>
+
+                </LayoutContent>
+
+                <LayoutContent>
+                  <Card>
+                <Link to={`/seepost/1`}>
+                    <Button src={AddPost} />
+                  <Description>ประเภท :<p/>ที่อยู่dssds</Description>
+                  </Link>
+                  </Card>
+
+                  <Card>
+                    <Link to={`/seepost/1`}>
+                    <Button src={AddPost} />
+                  <Description>ประเภท :<p/>ที่อยู่dssds</Description>
+                  </Link>
+                  </Card>
+
+                  <Card>
+                    <Link to={`/seepost/1`}>
+                    <Button src={AddPost} />
+                  <Description>ประเภท :<p/>ที่อยู่dssds</Description>
+                  </Link>
+                  </Card>
               </LayoutContent>
               
             </SemiSubtitle>
--- src/pages/Needroom.js
+++ src/pages/Needroom.js
@@ -8,12 +8,10 @@ import styled from "styled-components";
 
 const Container = styled.div`
   position: absolute;
-  width: 100%;
+  width: 60%;
   height: auto;
   background-color: #81c0c5;
-  display: flex;
-  flex: 1;
-  flex-direction: row;
+
 `;
 const Content = styled.div`
   display: flex;
@@ -26,14 +24,31 @@ const LayoutContent = styled.div`
   flex-direction: row;
   align-items: center;
   padding: 1rem 1rem;
+
 `;
 
 const Button = styled.button`
   display: flex;
+  flex:1;
+
+  width: 45%;
+  padding: 4rem;
+  margin: 2rem 2rem;
+  border: none;
+  background-image: url(${(props) => props.src});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+   cursor: pointer;
+`
+
+const AddButton = styled.button`
+display: flex;
+
 
   width: 45%;
   padding: 4.5rem;
-  margin: 2rem 5rem;
+  margin: 2rem 2.5rem;
   border: none;
   background-color: #4192b0;
   background-image: url(${(props) => props.src});
@@ -42,19 +57,31 @@ const Button = styled.button`
   background-position: center;
 
   &:hover {
-    filter: drop-shadow(1px 1px 10px #fff0cb);
+    filter: drop-shadow(1px 1px 10px  #fff0cb );
     cursor: pointer;
     transition: filter 0.5s;
   }
 `;
+const Card = styled.div`
 
+box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+
+  background-color:#004B62;
+  margin: 0 1rem;
+
+  &:hover{
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  }
+`
 const Description = styled.div`
-  padding: 2px 16px;
+  padding-bottom: 1rem;
+  padding-left: 1rem;
 `
 
 function Needroom() {
-  const [page, setPage] = useState();
-  const [dataAll, setDataAll] = useState();
+  const [page, setPage] = useState(1);
+  const [dataAll, setDataAll] = useState(null);
 
   useEffect(() => {
     getNeedpost(page).then((response) => setDataAll(response));
@@ -72,20 +99,61 @@ function Needroom() {
                 {" "}
                 หรือ
                 <a href="/haveroom"> กดที่นี่หากคุณมีห้องอยู่แล้ว</a>
+
                 <LayoutContent>
                   <Link to="/addpost">
-                    <Button src={AddPost} />
+                    <AddButton src={AddPost} />
                   </Link>
-                  {dataAll?.data.data.map((item, index) => (
+                  {/* {dataAll?.data.data.map((item, index) => (
                   <Link to={`/seepost/${index}`}>
                     <Button src={item.cover_img_url} />
                   <Description>ประเภท :{item.need_type}<p/>ที่อยู่{item.need_location}</Description>
                   </Link>
-                ))}
+                ))} */}
+                    <Card>
+                    <Link to={`/seepost/1`}>
+                    <Button src={AddPost} />
+                  <Description>ประเภท :<p/>ที่อยู่dssds</Description>
+                  </Link>
+                  </Card>
+
+                  <Card>
+                    <Link to={`/seepost/1`}>
+                    <Button src={AddPost} />
+                  <Description>ประเภท :<p/>ที่อยู่dssds</Description>
+                  </Link>
+                  </Card>
+
+                   
+
+
+                </LayoutContent>
+
+                <LayoutContent>
+                  <Card>
+                <Link to={`/seepost/1`}>
+                    <Button src={AddPost} />
+                  <Description>ประเภท :<p/>ที่อยู่dssds</Description>
+                  </Link>
+                  </Card>
+
+                  <Card>
+                    <Link to={`/seepost/1`}>
+                    <Button src={AddPost} />
+                  <Description>ประเภท :<p/>ที่อยู่dssds</Description>
+                  </Link>
+                  </Card>
+
+                  <Card>
+                    <Link to={`/seepost/1`}>
+                    <Button src={AddPost} />
+                  <Description>ประเภท :<p/>ที่อยู่dssds</Description>
+                  </Link>
+                  </Card>
                 </LayoutContent>
               </SemiSubtitle>
             </Content>
-            <Content></Content>
+
           </Container>
         </BackgroundBlock>
       </div>
